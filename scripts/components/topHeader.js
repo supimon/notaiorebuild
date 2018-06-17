6b23ea82d2362a411385835d8c0a3c8517024051
Supi <supimon@gmail.com>
2018-06-17 17:48:50 +0530

    added sass watch to the project

--- scripts/components/topHeader.js
+++ scripts/components/topHeader.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
-  $('.tooltipped').tooltip();
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems);
+  //var instance = M.Tooltip.getInstance(elems[1]);
+  setTimeout(function(){console.log(instances[0].el);}, 2000);
 });
       
